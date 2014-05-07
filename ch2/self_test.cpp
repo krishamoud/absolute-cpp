2034c9e0d2ad88376275dfcd260503935dec45f1
Kris Hamoud <khamoud@umich.edu>
2014-05-06 17:45:29 -0700

    ch2

---
+++ ch2/self_test.cpp
@@ -0,0 +1,45 @@
+/**
+ * Author: Kris Hamoud
+ * Purpose: Learning
+ * Book: Absolute C++
+ * Github: https://github.com/krishamoud
+ * Git Repo: https://github.com/krishamoud/absolute-cpp.git 
+ * Chapter: 2
+ * Questions: Self Test
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+	/* -- Question 1 Begin -- 
+	int count = 0, limit = 10;
+	bool truth;
+	truth = (count == 0) && (limit < 20); //true
+	cout << truth << endl;
+	truth = count == 0 && limit < 20; //true
+	cout << truth << endl;
+	truth = (limit > 20) || (count < 5); //true
+	cout << truth << endl;
+	truth = !(count == 12); // true
+	cout << truth << endl;
+	truth = (count == 1) && (limit < count); //false
+	cout << truth << endl;
+	truth = count < 10; //true
+	cout << truth << endl;
+	-- Question 1 End -- */
+	
+	/* -- Question 2 Begin -- */
+	//bool truth;
+	//int x;
+	//truth = x < 3 && x > 2;
+	/* -- Question 2 End -- */
+
+	/* -- Question 3 Begin -- */
+	/* -- Question 3 End -- */
+	//enum Direction  { N = 5, S = 7, E = 1, W};
+	//cout << W << " " << E << " " << S << " " << N << endl;
+	
+	return 0;
+}
