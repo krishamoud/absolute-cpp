dc1bf17a47fa39c75b26f404c6d57dc0c01c0cbf
Kris Hamoud <khamoud@umich.edu>
2014-05-12 11:00:32 -0700

    more structs n shit

--- ch6/self_test.cpp
+++ ch6/self_test.cpp
@@ -11,6 +11,14 @@
 #include<iostream>
 using namespace std;
 
+struct ShoeType
+{
+	char style;
+	double price;
+};
+
+void readShoeRecord(ShoeType& newShoe);
+void writeShoeRecord(ShoeType& newShoe);
 int main(){
 	/* -- Question 1 Begin -- */
 	//a)double
@@ -26,7 +34,48 @@ int main(){
 	/* -- Question 2 End -- */
 
 	/* -- Question 3 Begin -- */
-
+	//There is no semicolon after the structure definition
 	/* -- Question 3 End -- */
+
+	/* -- Question 4 Begin -- */
+	struct A
+	{
+ 		int b;
+ 		int c;
+	};
+	A x = {1, 2};
+	cout << x.b << " " << x.c << endl;
+	/* -- Question 4 End -- */
+
+	/* -- Question 5 Begin -- */
+	//A) month and day are initialized but not year
+	//B) month, day, and year are all initialized
+	//C) Error.  Too many initializers
+	/* -- Question 5 End -- */
+
+	/* -- Question 6 Begin -- */
+	struct EmployeeRecord{
+		double wage;
+		int vacation;
+		char status;
+	};
+	/* -- Question 6 End -- */
+
+	/* -- Question 7 Begin -- */
+	ShoeType shoe;
+	readShoeRecord(shoe);
+	writeShoeRecord(shoe);
+	/* -- Question 7 End -- */
 	return 0;
+}
+
+void readShoeRecord(ShoeType& newShoe){
+	cout << "What's type of shoe is it?: ";
+	cin >> newShoe.style;
+	cout << "What's the price of the shoe?: ";
+	cin >> newShoe.price;	
+}
+
+void writeShoeRecord(ShoeType& newShoe){
+	cout << "This is a " << newShoe.style << " style shoe and it costs $" << newShoe.price << ".\n";
 }
