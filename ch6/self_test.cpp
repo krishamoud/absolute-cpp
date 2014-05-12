c7db015ed5097951e2b517569991db425e3fb5ea
Kris Hamoud <khamoud@umich.edu>
2014-05-12 10:44:03 -0700

    structs n shit

---
+++ ch6/self_test.cpp
@@ -0,0 +1,32 @@
+/**
+ * Author: Kris Hamoud
+ * Purpose: Learning
+ * Book: Absolute C++
+ * Github: https://github.com/krishamoud
+ * Git Repo: https://github.com/krishamoud/absolute-cpp.git 
+ * Chapter: 6 
+ * Questions: Self Test
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	/* -- Question 1 Begin -- */
+	//a)double
+	//b)double
+	//c)int
+	//d)char
+	//e)struct
+	/* -- Question 1 End -- */
+
+	/* -- Question 2 Begin -- */
+	//A $9.99 
+	//A $9.99
+	/* -- Question 2 End -- */
+
+	/* -- Question 3 Begin -- */
+
+	/* -- Question 3 End -- */
+	return 0;
+}
